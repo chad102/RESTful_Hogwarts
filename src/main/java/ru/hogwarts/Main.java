5a31bbf13f3a1139dfbc0bc82e710cf479520aa6
Чингис <cianeed08@gmail.com>
2024-03-29 01:27:14 +0300

    create new project

---
+++ src/main/java/ru/hogwarts/Main.java
@@ -0,0 +1,9 @@
+package ru.hogwarts;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
